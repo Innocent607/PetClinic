74707ffcf349dd8b267ea469e144c1832a1cadd2
Innocent607 <68567585+Innocent607@users.noreply.github.com>
2021-05-18 08:51:56 +0200

    lsting owners from the database. Closes #16

--- pet-clinic-web/src/main/java/com/example/PetClinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/PetClinic/controllers/OwnerController.java
@@ -1,14 +1,26 @@
 package com.example.PetClinic.controllers;
 
+import com.example.PetClinic.model.Owner;
+import com.example.PetClinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public  String listOwners() {
+    public  String listOwners(Model model) {
+
+        //"owners" is a mapping property that out html page is used to query data
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
--- pet-clinic-web/src/main/java/com/example/PetClinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/PetClinic/controllers/VetController.java
@@ -1,13 +1,18 @@
 package com.example.PetClinic.controllers;
 
+import com.example.PetClinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+
+
     @RequestMapping({"/vets", "vets/index", "/vets/index.html"})
     public  String listVets() {
+
         return "vets/index";
     }
 }
