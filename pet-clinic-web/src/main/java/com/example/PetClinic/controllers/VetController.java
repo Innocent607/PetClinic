4dd69e2d35167d5ac38d32050f9856c097218f32
Innocent607 <68567585+Innocent607@users.noreply.github.com>
2021-05-18 08:53:48 +0200

    lsting vets from the database. Closes #15

--- pet-clinic-web/src/main/java/com/example/PetClinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/PetClinic/controllers/VetController.java
@@ -8,10 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"/vets", "vets/index", "/vets/index.html"})
-    public  String listVets() {
+    public  String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
