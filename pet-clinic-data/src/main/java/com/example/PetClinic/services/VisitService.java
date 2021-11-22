a55eab3b3f930bab48dc3583936c379b783bfbc5
Innocent607 <68567585+Innocent607@users.noreply.github.com>
2021-11-22 12:42:08 +0200

    visit map service. closes #47

---
+++ pet-clinic-data/src/main/java/com/example/PetClinic/services/VisitService.java
@@ -0,0 +1,11 @@
+package com.example.PetClinic.services;
+
+import com.example.PetClinic.model.Visit;
+
+/**
+ * Created by Onalethata I. L. Maswabi
+ * AKA Inno607
+ */
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
